9001103c88d8ac01e9225a352a8794fda772b03c
KAMADA Ken'ichi <kamada@nanohz.org>
2017-03-26 21:29:33 +0900

    Add compatibility notes in the documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,16 @@
 //!     }
 //! }
 //! ```
+//!
+//! # Compatibility
+//!
+//! Major changes between 0.1.3 and 0.2 follow.
+//!
+//! * Rust 1.15 is now required to compile.
+//! * `Reader.fields()` now returns a slice instead of a reference to a Vec.
+//! * `Tag.value()` was renamed to `Tag.code()`.
+//! * A new variant `Error::BlankValue` has been added.
+//! * The `parse_image()` function has been removed.
 
 pub use error::Error;
 pub use jpeg::get_exif_attr as get_exif_attr_from_jpeg;
